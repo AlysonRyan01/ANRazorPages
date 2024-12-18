6276f6dd3b2fa2414a0cee2057c853a6d5185762
AlysonRyan01 <sdhuahdsaih@gmail.com>
2024-12-18 15:28:45 -0300

    Initial

---
+++ Pages/Login.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ANRazorPages.Pages;
+
+public class Login : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ Pages/Shared/NavMenuPartial.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ANRazorPages.Pages.Shared;
+
+public class NavMenuPartial : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
