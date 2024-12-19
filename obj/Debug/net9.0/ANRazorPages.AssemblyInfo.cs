b53cb96854fb0233967514e99261a4bfc4779fbe
Alyson Ryan Ullirsch <sdhuahdsaih@gmail.com>
2024-12-18 21:31:06 -0300

    done

--- obj/Debug/net9.0/ANRazorPages.AssemblyInfo.cs
+++ obj/Debug/net9.0/ANRazorPages.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ANRazorPages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6276f6dd3b2fa2414a0cee2057c853a6d5185762")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b83b34c2de25a7432cf15f01edf274fa2e72d50e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ANRazorPages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ANRazorPages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
